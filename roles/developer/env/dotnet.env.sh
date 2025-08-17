4379eaacddb3147045f97c712a472f712e7b5f93
southpawriter02 <207498890+southpawriter02@users.noreply.github.com>
2025-08-17 10:32:14 -0600

    Added .NET configuration automation

---
+++ roles/developer/env/dotnet.env.sh
@@ -0,0 +1,10 @@
+# ==============================================================================
+#
+# FILE:         dotnet.env.sh
+#
+# DESCRIPTION:  Environment variables for .NET Core SDK.
+#
+# ==============================================================================
+
+# Add .NET Core tools to the PATH
+export PATH="$HOME/.dotnet/tools:$PATH"
