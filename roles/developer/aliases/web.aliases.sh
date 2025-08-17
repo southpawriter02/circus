d15fd83e4d656dcf26617875bbae6112b9bb46e4
southpawriter02 <207498890+southpawriter02@users.noreply.github.com>
2025-08-17 10:33:27 -0600

    Added web development aliases

---
+++ roles/developer/aliases/web.aliases.sh
@@ -0,0 +1,18 @@
+# ==============================================================================
+#
+# FILE:         web.aliases.sh
+#
+# DESCRIPTION:  Aliases for web development, including nvm and npm.
+#
+# ==============================================================================
+
+# nvm (Node Version Manager)
+alias nvmls="nvm ls"
+alias nvmlsr="nvm ls-remote"
+alias nvmu="nvm use"
+alias nvmi="nvm install"
+
+# npm (Node Package Manager)
+alias npmi="npm install"
+alias npmu="npm uninstall"
+alias npmr="npm run"
