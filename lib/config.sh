17f3c6b7977b30c5952f0cfef947c303d841e7bf
southpawriter02 <207498890+southpawriter02@users.noreply.github.com>
2025-08-16 19:07:55 -0600

    Added central configuration file

---
+++ lib/config.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# ==============================================================================
+#
+# FILE:         lib/config.sh
+#
+# DESCRIPTION:  This file serves as the central configuration hub for the
+#               Dotfiles Flying Circus installer. All user-configurable
+#               variables should be defined here.
+#
+# CUSTOMIZATION:
+#
+# To customize your installation, modify the variables in this file. This
+# allows you to easily change which packages are installed, where files are
+# located, and other installer behaviors without having to modify the core
+# installation logic.
+#
+# ==============================================================================
+
+#
+# Example Configuration Variable
+#
+# This is an example of how you might define a list of essential Homebrew
+# packages that you always want to have installed.
+#
+# readonly CORE_BREW_PACKAGES=(
+#   "git"
+#   "wget"
+#   "htop"
+# )
